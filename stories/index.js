68c86055b21483f0d2376dc19644496812ff559b
Michael Wagner <mdwranger@gmail.com>
2018-03-20 23:23:25 -0400

    angularjs and storybook working great together

---
+++ stories/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import angular from 'angular';
+import { storiesOf } from '@storybook/react';
+import { withNotes } from '@storybook/addon-notes';
+import { withConsole } from '@storybook/addon-console';
+import AngularBootstrap from './AngularBootstrap';
+import something from './something.md';
+
+angular.module('myModule', [])
+  .controller('Ctrl', [
+    class {
+      constructor() {
+        this.firstName = '';
+      }
+      clickme() {
+        this.firstName = 'Wagz';
+        console.log('clickme() clicked');
+      }
+    }
+  ])
+  .component('widget', {
+    template: '<button ng-click="$ctrl.onClickHandler()">{{$ctrl.lastname}}</button>',
+    controller: class {
+      onClickHandler() {
+        console.log('onClickHandler() clicked');
+      }
+    },
+    bindings: {
+      lastname: '<'
+    }
+  });
+
+storiesOf('Button', module)
+  .addDecorator((storyFn, context) => withConsole()(storyFn)(context))
+  .add('with ng-controller', () => (
+    <AngularBootstrap
+      ng-controller="Ctrl as vm"
+      template={`
+        <button ng-click="vm.clickme()">Click Me! {{vm.firstName}}</button>
+      `}
+      moduleToBootstrap="myModule"
+      />
+  ))
+  .add('with no ng-controller', withNotes(something)(() => (
+    <AngularBootstrap
+      template={`
+        <widget lastname="'mike'"></widget>
+      `}
+      moduleToBootstrap="myModule"
+      />
+  )));
+
---
+++ stories/something.md
@@ -0,0 +1,1 @@
+**Something** _goes_ here
